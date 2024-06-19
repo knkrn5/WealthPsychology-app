8d3b5465b8febd4b5fd1c748a4a840ea51a54ded
Karan <xkaranx5@gmail.com>
2024-06-19 10:55:06 +0530

    made the sleek hamburger menu in all the pages

---
+++ navbar/nav.js
@@ -0,0 +1,13 @@
+
+
+// global navigation js------------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('.nav-menu');
+  
+    mobileMenu.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+    });
+  });
+  
