bf45f1ab25090406fee32f54d0d9a78931c3f0a2
Karan <xkaranx5@gmail.com>
2024-06-22 10:38:23 +0530

    X transition adding

--- navbar/nav.js
+++ navbar/nav.js
@@ -3,11 +3,11 @@
 // global navigation js------------------------------------
 
 document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navMenu = document.querySelector('.nav-menu');
-  
-    mobileMenu.addEventListener('click', function() {
-      navMenu.classList.toggle('active');
-    });
+  const mobileMenu = document.getElementById('mobile-menu');
+  const navMenu = document.querySelector('.nav-menu');
+
+  mobileMenu.addEventListener('click', function() {
+      navMenu.classList.toggle('active'); // Toggle the menu visibility as well
+      mobileMenu.classList.toggle('active'); // Toggle class on mobileMenu
   });
-  
+});
