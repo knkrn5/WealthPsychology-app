34a35afc4709cb4ea509c8fa5ab6f66bbae6cdaf
Karan <xkaranx5@gmail.com>
2024-06-22 10:41:09 +0530

    X hamburger adding

--- home page/script.js
+++ home page/script.js
@@ -1,13 +1,17 @@
+
+
 // script.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navMenu = document.querySelector('.nav-menu');
-  
-    mobileMenu.addEventListener('click', function() {
-      navMenu.classList.toggle('active');
-    });
+  const mobileMenu = document.getElementById('mobile-menu');
+  const navMenu = document.querySelector('.nav-menu');
+
+  mobileMenu.addEventListener('click', function() {
+      navMenu.classList.toggle('active'); // Toggle the menu visibility as well
+      mobileMenu.classList.toggle('active'); // Toggle class on mobileMenu
   });
+});
+
   
 
 
