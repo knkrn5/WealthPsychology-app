d8a98d44724659ab3f4c3385dd247154e7db30b9
Karan <xkaranx5@gmail.com>
2024-06-27 21:54:18 +0530

    footer updataed

--- footer/footer.js
+++ footer/footer.js
@@ -21,8 +21,8 @@ function highlightActiveLink() {
     const currentPath = decodeURIComponent(window.location.pathname); // Decode the current path
     console.log("Current Path: ", currentPath); // Debug log
 
-    const navLinks = document.querySelectorAll('.footer-link');
-    console.log("Footer Links: ", navLinks); // Debug log
+    const navLinks = document.querySelectorAll('.nav-link');
+    console.log("Nav Links: ", navLinks); // Debug log
 
     navLinks.forEach(link => {
         const linkPath = decodeURIComponent(link.getAttribute('href')); // Decode the link href
@@ -34,7 +34,6 @@ function highlightActiveLink() {
         }
     });
 }
-
 
 
 
