97b5c89243e46b3b28955ce8598ff181a4d2452a
Karan <xkaranx5@gmail.com>
2024-07-16 16:37:30 +0530

    js upadtes

--- home/script.js
+++ home/script.js
@@ -1,19 +1,8 @@
 
+//mobile nav menu bar via global nav.js-------------------------------
 
-// script.js
-
-document.addEventListener('DOMContentLoaded', function() {
-  const mobileMenu = document.getElementById('mobile-menu');
-  const navMenu = document.querySelector('.nav-menu');
-
-  mobileMenu.addEventListener('click', function() {
-      navMenu.classList.toggle('active'); // Toggle the menu visibility as well
-      mobileMenu.classList.toggle('active'); // Toggle class on mobileMenu
-  });
-});
 
   
-
 
 // new feature prompt javascript------------------
 document.addEventListener('DOMContentLoaded', function() {
