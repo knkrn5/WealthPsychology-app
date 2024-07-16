3e59a8780d601457a336c024b439da16c5e14b8b
Karan <xkaranx5@gmail.com>
2024-07-16 16:28:42 +0530

    updated nav toggling

--- navbar/nav.js
+++ navbar/nav.js
@@ -3,11 +3,18 @@
 // global navigation js------------------------------------
 
 document.addEventListener('DOMContentLoaded', function() {
-  const mobileMenu = document.getElementById('mobile-menu');
+  const menuBars = document.getElementById('menu-bars');
   const navMenu = document.querySelector('.nav-menu');
 
-  mobileMenu.addEventListener('click', function() {
+  menuBars.addEventListener('click', function() {
       navMenu.classList.toggle('active'); // Toggle the menu visibility as well
-      mobileMenu.classList.toggle('active'); // Toggle class on mobileMenu
+      menuBars.classList.toggle('active'); // Toggle class on mobileMenu
+  });
+});
+
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+      navMenu.classList.remove('active'); // Hide the menu
+      menuBars.classList.remove('active'); // Remove active class from mobileMenu
   });
 });
