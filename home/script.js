05501af967b0120ac7177538c24ed7215ef1c533
Karan <xkaranx5@gmail.com>
2024-08-02 13:58:47 +0530

    updaed full screen

--- home/script.js
+++ home/script.js
@@ -1,17 +1,18 @@
 
 //mobile nav menu bar via global nav.js-------------------------------
 
-
-  
-
-// new feature prompt javascript------------------
-document.addEventListener('DOMContentLoaded', function() {
-  if (!localStorage.getItem('dismissedFeaturePrompt')) {
-    document.getElementById('newFeaturePrompt').style.display = 'block';
+// new feature prompt javascript
+function dismissPrompt(event) {
+  const prompt = document.getElementById('newFeaturePrompt');
+  if (prompt) {
+    prompt.remove();
   }
-});
-
-function dismissPrompt() {
-  localStorage.setItem('dismissedFeaturePrompt', true);
-  document.getElementById('newFeaturePrompt').style.display = 'none';
 }
+
+// Add click event listener to the entire document
+document.addEventListener('click', dismissPrompt);
+
+// Add click event listener to the prompt itself to prevent event bubbling
+document.getElementById('newFeature')?.addEventListener('click', (event) => {
+  event.stopPropagation(); // Prevents click events from bubbling up to the document
+});
