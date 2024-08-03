e6f1ac111dd25827a8d68aa47520a4348ceacc27
Karan <xkaranx5@gmail.com>
2024-08-03 13:08:40 +0530

    updated

--- navbar/nav.js
+++ navbar/nav.js
@@ -6,8 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuBars = document.getElementById('menu-bars');
   const navMenu = document.querySelector('.nav-menu');
 
-  document.addEventListener('click', function() { //whole screen click closure
-      navMenu.classList.toggle('active'); // Toggle class on mobile nav Menu option
-      menuBars.classList.toggle('active'); // Toggle the menu bar visibility 
+  // Toggle the menu visibility when clicking the menu bar
+  menuBars.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent the click event from bubbling up to the document
+      navMenu.classList.toggle('active'); // Toggle the menu visibility
+      menuBars.classList.toggle('active'); // Toggle the class on the menu bar
+  });
+
+  // Hide the menu when clicking outside the menu bar and menu
+  document.addEventListener('click', function(event) {
+      if (!navMenu.contains(event.target) && !menuBars.contains(event.target)) {
+          navMenu.classList.remove('active'); // Hide the menu
+          menuBars.classList.remove('active'); // Remove the active class from the menu bar
+      }
   });
 });
