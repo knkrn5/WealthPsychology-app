dfb24bef2927e6118fc96719697f7ed62a5ffa72
Karan <xkaranx5@gmail.com>
2024-08-03 12:51:38 +0530

    updated

--- navbar/nav.js
+++ navbar/nav.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const navMenu = document.querySelector('.nav-menu');
 
   document.addEventListener('click', function() { //whole screen click closure
-      navMenu.classList.toggle('active'); // Toggle the menubar visibility
-      menuBars.classList.toggle('active'); // Toggle class on mobileMenu
+      navMenu.classList.toggle('active'); // Toggle class on mobile nav Menu option
+      menuBars.classList.toggle('active'); // Toggle the menu bar visibility 
   });
 });
