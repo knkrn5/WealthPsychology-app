0780dfef68c382abe6679728ef1c0a202214594c
Karan <xkaranx5@gmail.com>
2024-08-03 12:47:49 +0530

    updated

--- navbar/nav.js
+++ navbar/nav.js
@@ -6,15 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuBars = document.getElementById('menu-bars');
   const navMenu = document.querySelector('.nav-menu');
 
-  menuBars.addEventListener('click', function() {
-      navMenu.classList.toggle('active'); // Toggle the menu visibility as well
+  document.addEventListener('click', function() { //whole screen click closure
+      navMenu.classList.toggle('active'); // Toggle the menubar visibility
       menuBars.classList.toggle('active'); // Toggle class on mobileMenu
   });
 });
-
-/* navLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-      navMenu.classList.remove('active'); // Hide the menu
-      menuBars.classList.remove('active'); // Remove active class from mobileMenu
-  });
-}); */
