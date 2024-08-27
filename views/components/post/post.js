f78d5b577034a6e762f503e536bfd597ed98028b
Karan <xkaranx5@gmail.com>
2024-08-27 14:38:08 +0530

    added margin in post

--- views/components/post/post.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const articleElement = document.querySelector('#post-content .full-article');
-    const loadingContainer = document.getElementById('loading-container');
-
-    // Hide loading indicator and show content
-    loadingContainer.style.display = 'none';
-    articleElement.style.display = 'block';
-});
