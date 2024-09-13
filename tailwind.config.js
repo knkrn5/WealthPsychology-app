57da50245b3ab018af5e38a271f28ef14e2496dc
Karan <xkaranx5@gmail.com>
2024-09-13 15:43:31 +0530

    Refactor tailwind.config.js to exclude node_modules from content

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './**/*.{html,js}', // Matches all files in the project, excluding node_modules
+    '!./node_modules/**', // Exclude node_modules
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false, // Disable Tailwind's Preflight (base/reset) styles
+  },
+  plugins: [],
+};
