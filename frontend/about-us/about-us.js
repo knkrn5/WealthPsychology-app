8ea604701ebd9742ec89a0592c7d2d553e0c27b5
Karan <xkaranx5@gmail.com>
2024-11-09 16:26:29 +0530

    about us changed completely

--- frontend/about-us/about-us.js
+++ frontend/about-us/about-us.js
@@ -1,29 +1,4 @@
-
-
-
-function handleFormSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-    var myPnL = document.getElementById('mypnl').value;
-    var redirectUrl;
-
-    // Define URLs for each option
-    switch (myPnL) {
-        case 'value1':
-            redirectUrl = 'https://docs.google.com/spreadsheets/d/1sy5YNpgrk0VcZdWFdWS7KFHzsDOuDFKbOZnUyOzAf6I/edit?usp=sharing';
-            break;
-        case 'value2':
-            redirectUrl = 'https://docs.google.com/spreadsheets/d/1sy5YNpgrk0VcZdWFdWS7KFHzsDOuDFKbOZnUyOzAf6I/edit?usp=sharing';
-            break;
-        case 'value3':
-            redirectUrl = 'https://docs.google.com/spreadsheets/d/1sy5YNpgrk0VcZdWFdWS7KFHzsDOuDFKbOZnUyOzAf6I/edit?usp=sharing';
-            break;
-        default:
-            alert('Please select a valid P&L statement.')
-            redirectUrl = null; // Ensure redirectUrl is null for default case
-            break;
-    }
-
-    if (redirectUrl) {
-        window.open(redirectUrl, '_blank');  // Open the specified URL in a new tab
-    }
-}
+// Placeholder for contact button functionality
+document.getElementById("contact-us-button").addEventListener("click", () => {
+    window.location.href = "/contact-us/";
+});
