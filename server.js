8d71d3cd916c906a6c4ade6048593bd81e7fe0f7
Karan <xkaranx5@gmail.com>
2025-03-21 12:29:07 +0530

    baseurl

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_SECRET,
-  baseURL: 'http://localhost:55555',
+  // baseURL: 'http://localhost:55555',
+  baseURL: 'https://wealthpsychology.in',
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: 'https://dev-ze43n30i2zjn5fuz.us.auth0.com',
 
